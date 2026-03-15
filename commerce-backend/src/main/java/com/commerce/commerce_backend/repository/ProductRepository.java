0ce89b5a66df0eed46dad3bc3c5ec391789e1f32
Hoai Nhu <nguyenthihoainhu07@gmail.com>
2026-03-15 17:46:40 +0700

    remove unused files

--- commerce-backend/src/main/java/com/commerce/commerce_backend/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.commerce.commerce_backend.repository;
-
-import com.commerce.commerce_backend.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
